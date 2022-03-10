c1ee4e274d51de514bae98f2cb11da17f36c740f
Giovanna Niemeyer <giovanna.assis@loggi.com>
2022-03-10 10:00:41 -0300

    starting models

--- repoprovas/models.py
+++ repoprovas/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.category
+
+
+class Test(models.Model):
+    semester = models.CharField(max_length=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    file = models.CharField(max_length=200)
+
+    def publish(self):
+        self.save()
